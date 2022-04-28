73257d38cb89f47cf69ff29c2316e2c41ef1bfa8
Aitorbp1 <101257184+Aitorbp1@users.noreply.github.com>
2022-04-28 08:58:21 +0200

    doble salt
    salt doble

--- Escenes/MASK DUDEEE.gd
+++ Escenes/MASK DUDEEE.gd
@@ -1,10 +1,11 @@
 extends KinematicBody2D
 
-var velocitat_base = 200 #la velocitat inicial
+var velocitat_base = 300 #la velocitat inicial
 var velocitat = Vector2.ZERO
 var direccio = Vector2.DOWN
 var gravetat = Vector2.DOWN * 980
-var velocitat_salt = -340
+var velocitat_salt = -300
+var salts = 1
 #var velocitat_avall = 1000
 
 func _physics_process(delta):
@@ -14,8 +15,13 @@ func _physics_process(delta):
 		velocitat += Vector2.RIGHT * velocitat_base
 	if Input.is_action_pressed("Left 2"):
 		velocitat += Vector2.LEFT * velocitat_base
-	if Input.is_action_just_pressed("Up 2") and is_on_floor():
-		velocitat.y = velocitat_salt
+	if Input.is_action_just_pressed("Up 2"):
+		if salts > 0:
+			velocitat.y = velocitat_salt
+			salts -= 1
+	if is_on_floor():
+		salts = 1
+		
 	#if Input.is_action_just_pressed("Down 2") and is_on_floor():
 		#velocitat.y = velocitat_avall
 		
